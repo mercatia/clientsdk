844c2724526fbd6ab78cc0999e6a5ee1e474796e
mbw <matthew@pop-os.localdomain>
2023-06-12 11:18:48 +0100

    Starting
    Signed-off-by: mbw <matthew@pop-os.localdomain>

---
+++ clientsdk/simulation.py
@@ -0,0 +1,52 @@
+
+def class StockMarketSimulation:
+    def __init__ ():
+
+    def market():
+
+
+
+def class Market:
+
+    def __init__(self):
+#   "id" : 1,
+#   "date" : "1/4/1",
+#   "lastMonthAveragePrice" : 100.0,
+#   "marketTrajectory" : "NORMAL"
+
+
+def class Stock:
+
+    
+    @property
+    def ticker(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value.upper()
+
+
+    {
+  "ticker" : "AAPL",
+  "companyName" : "Apple",
+  "sector" : "Technology",
+  "marketCap" : "Large",
+  "price" : 99.742,
+  "lastDayPrice" : 99.457,
+  "optimism" : -1,
+  "volatileStock" : false
+}
+
+def class StockHistory:
+
+def class StockNews:
+
+def class StockEarnings:
+
+def class IndexFund:
+
+def class Account:
+
+def class Leaderboard:    
+
